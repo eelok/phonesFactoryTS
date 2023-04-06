69111c61a89ac33589ad1670f9e60eaf7213574b
eelok <maria@resourceful-humans.de>
2023-04-06 20:19:01 +0200

    complete updateManufacturer

--- src/controller/manufacturer.ts
+++ src/controller/manufacturer.ts
@@ -1,27 +1,27 @@
-import express, {Express, Request, Response} from 'express';
-import {Manufacturer, Location} from './ManufacturerType';
-import {statusCodes} from '../statusCodes';
+import express, { Express, Request, Response } from 'express';
+import { Manufacturer, Location } from './ManufacturerType';
+import { statusCodes } from '../statusCodes';
 
 let storage: Record<string, Manufacturer> = {};
 //todo write unit test when manufacturer is null
 export const createManufacturer = (req: Request, res: Response): void => {
     let manufacturer: Manufacturer = req.body;
-    if(!manufacturer || typeof manufacturer !== "object"){
+    if (!manufacturer || typeof manufacturer !== "object") {
         res.status(statusCodes.BAD_REQUEST).send('Invalid manufacturer object');
         return;
     }
-    let {name} = manufacturer;
+    let { name } = manufacturer;
     if (!name || !name.trim()) {
         res.status(statusCodes.BAD_REQUEST).send('Manufacturer name is required');
         return;
     }
     name = name.trim().toLowerCase();
-    if(Object.keys(storage).length === 0) {
+    if (Object.keys(storage).length === 0) {
         storage[name] = manufacturer;
         res.status(statusCodes.CREATED).send(manufacturer);
         return;
     }
-    if(storage[name]){
+    if (storage[name]) {
         res.status(statusCodes.BAD_REQUEST).send('Manufacturer already exists');
         return;
     }
@@ -31,20 +31,20 @@ export const createManufacturer = (req: Request, res: Response): void => {
 
 export const getManufacturerByName = (req: Request, res: Response): void => {
     let name: string = req.params.name;
-    if(!name || !name.trim().toLowerCase()){
+    if (!name || !name.trim().toLowerCase()) {
         res.status(statusCodes.BAD_REQUEST).send('name is required');
         return;
     }
     name = name.trim().toLowerCase()
     const manufacturer = storage[name];
-    if(!manufacturer){
+    if (!manufacturer) {
         res.status(statusCodes.BAD_REQUEST).send('Manufacturer with the name ${name} does not exists');
         return;
     }
     res.status(statusCodes.OK).send(manufacturer);
 }
 
-export const getAllManufacturers = (req: Request, res: Response): void  => {
+export const getAllManufacturers = (req: Request, res: Response): void => {
     res.status(statusCodes.OK).send(storage);
 }
 
@@ -55,37 +55,38 @@ export const deleteManufacturerByName = (req: Request, res: Response): void => {
         return;
     }
     manufacturerName = manufacturerName.trim().toLowerCase();
-    const manufacturer =  storage[manufacturerName];
-    if(!manufacturer){
+    const manufacturer = storage[manufacturerName];
+    if (!manufacturer) {
         res.status(statusCodes.NOT_FOUND).send(`Manufacturer with a name ${manufacturerName} was not found`);
         return;
     }
-    if(!delete storage[manufacturerName]){
+    if (!delete storage[manufacturerName]) {
         res.status(statusCodes.INTERNAL_SERVER_ERROR).send();
         return;
     }
     res.status(statusCodes.OK).send();
     return;
 }
-//todo wip
+
 export const updateManufacturer = (req: Request, res: Response): void => {
-    let {name, location: {country, city}}: Manufacturer = req.body;
+    let { name, location: { country, city } }: Manufacturer = req.body;
     if (!name || !name.trim()) {
         res.status(statusCodes.BAD_REQUEST).send('Manufacturer name is required');
         return;
     }
     name = name.trim().toLowerCase()
     let foundManufacturer = storage[name];
-    if(!foundManufacturer){
+    if (!foundManufacturer) {
         res.status(statusCodes.NOT_FOUND).send(`Manufacturer with a name ${name} was not found`);
         return;
     }
-    if(country && country.trim()){
+    if (country && country.trim()) {
         country = country.toLowerCase();
         foundManufacturer.location.country = country;
     }
-    if(city && city.trim()){
+    if (city && city.trim()) {
         city = city.toLowerCase();
         foundManufacturer.location.city = city;
     }
+    res.status(statusCodes.OK).send(foundManufacturer);
 }
